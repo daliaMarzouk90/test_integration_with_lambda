de48df29e02c8b8762c37b90d0bcf96c4d4d29d6
Dalia Marzouk <69721475+daliaMarzouk90@users.noreply.github.com>
2022-12-06 12:08:47 +0200

    get environment variables

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,14 @@
 import json
 import sys
 
+my_input = os.environ["INPUT_MYINPUT"]
+
 def lambda_handler(event, context):
     # TODO implement
     print("****************Start*****************************")
+    my_input = os.environ["INPUT_MYINPUT"]
+    my_output = f"Hello {my_input}"
+    print(f"::set-output name=myOutput::{my_output}")
     print("****************End*****************************")
     return {
           "isBase64Encoded": False,
